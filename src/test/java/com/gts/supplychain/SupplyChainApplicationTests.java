c92f855ae460ec60df64983d8d13eb6fcb64da7a
Milad <m.heydariyan@vasl.ir>
2025-09-10 16:19:27 +0330

    chore: setup project and add pom.xml

---
+++ src/test/java/com/gts/supplychain/SupplyChainApplicationTests.java
@@ -0,0 +1,14 @@
+package com.gts.supplychain;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplyChainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
